d48763740d1a2a5d8e1c2c73fe5297a9ab940f4a
scottgray76 <scottgray76@localhost>
2014-09-14 19:32:01 -0400

    didn't actually fully check in all changes last one..

--- sgemm/sgemm.pl
+++ sgemm/sgemm.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 use strict;
 
-print `maxas.pl -p sgemm2.sass sgemm_preprocessed.sass`;
+print `maxas.pl -p sgemm.sass sgemm_preprocessed.sass`;
 
-print `maxas.pl -p -r sgemm2.sass sgemm_preprocessed_regmap.sass`;
+print `maxas.pl -p -r sgemm.sass sgemm_preprocessed_regmap.sass`;
 
-print `maxas.pl -i sgemm2.sass sgemm.cubin`;
+print `maxas.pl -i sgemm.sass sgemm.cubin`;
 
 exit if $?;
 
@@ -13,7 +13,7 @@ print `maxas.pl -e sgemm.cubin sgemm_extracted.sass`;
 
 #print `Release\\sgemm.exe $_ 5` foreach (40,30,20,10,9,8,7,6,5,4,3,2,1);
 
-print `Release\\sgemm.exe 40 20`
+print `Release\\sgemm.exe 40 1`
 
 __END__
 
